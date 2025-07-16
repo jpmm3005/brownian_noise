d0373b1ceb093b36de77cfa9e6b75053364fd159
JP <JPMM3005@gmail.com>
2025-07-16 10:03:41 -0700

    fix

--- analytics.py
+++ analytics.py
@@ -6,7 +6,7 @@ from spotipy.oauth2 import SpotifyOAuth
 
 from spotipy import Spotify
 
-access_token = ''
+access_token = 'BQD2T6GueFt_5KU77n1_zCtDzdG-Z2TLGcSPj_c1X_MAQZEsC0f-TMx0HL01hG5Fk7Dlj7OLykNz9HyVq0CEMOX_HTau04-oz142nPVE0r6zyXa0HN2WlBbu7yiwcjJndy790qgSmijF6mgD5RTnHXpYpoURb8SojEtqEOKjYIP-EPUc5qzhem3EH3vVziM8Zqsc6FQl0yXC-LWSw5RkhTRa0YVbtcDFVOtvTkMdJlaR6hW1BvUCNZ0eiQ'
 sp = Spotify(auth=access_token)
 #https://accounts.spotify.com/en/authorize?response_type=code&client_id=b64fd8d41ec347fc9920a1549e265fcc&scope=user-top-read&redirect_uri=https%3A%2F%2Fjpmm3005.github.io%2Fbrownian_noise%2F&code_challenge_method=S256&code_challenge=dipfWgeRnRTGvH0pJhb_j8SgEa47YedZH2MqVTHlg_U
 
@@ -27,18 +27,3 @@ features_df.to_csv('static/audio_features.csv', index=False)
 
 print("Saved audio features to 'static/audio_features.csv'")
 
-
-
-
-'''
-df = pd.read_csv('static/top_tracks.csv')
-
-#print(str(df.head()))
-
-
-id_list = df.iloc[:,0]
-for track_id in id_list:
-    audio_features = 
-
-print(audio_features)
-'''
